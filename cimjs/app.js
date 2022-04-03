1d28a0417819e2ee4b805ad8ef5a8374652523ba
Elmo Neto <elmossneto@gmail.com>
2022-04-03 00:01:55 -0300

    desativação de página com exemplo de cimjs

--- cimjs/app.js
+++
@@ -1,9 +0,0 @@
-import {obtemCodigoCIM} from "./calculo_cim.js";
-
-function mostraResultado(){
-    let latitude = parseFloat(document.getElementById("lat").value);
-    let longitude = parseFloat(document.getElementById('lon').value);
-    var resultado = document.getElementById('resultado');
-    let codigo = obtemCodigoCIM(latitude,longitude);
-    resultado.innerHTML = codigo;
-}
